cd47fca4abb7f5ecb534b3b2aaff4a4b3f57153f
wy198908 <wy198908@gmail.com>
2013-09-19 02:35:42 +0000

    参数前缀绑定测试

--- easy/src/main/java/com/easy/admin/controller/IndexController.java
+++ easy/src/main/java/com/easy/admin/controller/IndexController.java
@@ -3,6 +3,12 @@
  */
 package com.easy.admin.controller;
 
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.easy.admin.entity.Admin;
 import com.easy.core.controller.BaseController;
 
 /**
@@ -12,5 +18,16 @@ import com.easy.core.controller.BaseController;
  * @version v 0.1 2013-9-15 下午12:53:19 wy Exp $
  */
 public class IndexController extends BaseController {
+
+	@InitBinder("admin")
+	public void initBinder(WebDataBinder binder) {
+		// 别名前缀
+		binder.setFieldDefaultPrefix("admin.");
+	}
+
+	public String index(ModelMap model, @ModelAttribute("admin") Admin admin) {
+
+		return null;
+	}
 
 }
