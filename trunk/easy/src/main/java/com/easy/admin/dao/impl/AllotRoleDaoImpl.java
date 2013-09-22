6d837fc0b53904d63db722d61b2dc8b83ffa7549
wy198908@gmail.com <wy198908@gmail.com@ffdc06e0-7b47-fd80-da87-33495e43a382>
2013-09-22 17:40:01 +0000

    多对多删除优化

--- trunk/easy/src/main/java/com/easy/admin/dao/impl/AllotRoleDaoImpl.java
+++ trunk/easy/src/main/java/com/easy/admin/dao/impl/AllotRoleDaoImpl.java
@@ -19,54 +19,55 @@ import com.easy.core.dao.impl.MyBatisGenericDao;
  */
 public class AllotRoleDaoImpl extends MyBatisGenericDao implements AllotRoleDao {
 
-	/**
-	 * @see com.easy.admin.dao.AllotRoleDao#create(com.easy.admin.entity.AllotRole)
-	 */
-	@Override
-	public AllotRole create(AllotRole allotRole) {
-		save("com.easy.dao.AllotRoleDao.create", allotRole);
-		return allotRole;
-	}
+    /**
+     * @see com.easy.admin.dao.AllotRoleDao#create(com.easy.admin.entity.AllotRole)
+     */
+    @Override
+    public AllotRole create(AllotRole allotRole) {
+        save("com.easy.dao.AllotRoleDao.create", allotRole);
+        return allotRole;
+    }
 
-	/**
-	 * @see com.easy.admin.dao.AllotRoleDao#update(com.easy.admin.entity.AllotRole)
-	 */
-	@Override
-	public int update(AllotRole allotRole) {
-		return update("com.easy.dao.AllotRoleDao.update", allotRole);
-	}
+    /**
+     * @see com.easy.admin.dao.AllotRoleDao#update(com.easy.admin.entity.AllotRole)
+     */
+    @Override
+    public int update(AllotRole allotRole) {
+        return update("com.easy.dao.AllotRoleDao.update", allotRole);
+    }
 
-	/**
-	 * @see com.easy.admin.dao.AllotRoleDao#getByPrimaryKey(java.lang.Long,
-	 *      java.lang.Long)
-	 */
-	@Override
-	public AllotRole getByPrimaryKey(Long userId, Long roleId) {
+    /**
+     * @see com.easy.admin.dao.AllotRoleDao#getByPrimaryKey(java.lang.Long,
+     *      java.lang.Long)
+     */
+    @Override
+    public AllotRole getByPrimaryKey(Long userId, Long roleId) {
 
-		Map<String, Object> params = new HashMap<String, Object>();
-		params.put("userId", userId);
-		params.put("roleId", roleId);
-		return get("com.easy.dao.AllotRoleDao.getByPrimaryKey", params);
-	}
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("userId", userId);
+        params.put("roleId", roleId);
+        return get("com.easy.dao.AllotRoleDao.getByPrimaryKey", params);
+    }
 
-	/**
-	 * @see com.easy.admin.dao.AllotRoleDao#deleteByPrimaryKeys(java.lang.Long,
-	 *      java.lang.Long)
-	 */
-	@Override
-	public int deleteByPrimaryKeys(Long userId, Long roleId) {
-		Map<String, Object> params = new HashMap<String, Object>();
-		params.put("userId", userId);
-		params.put("roleId", roleId);
-		return get("com.easy.dao.AllotRoleDao.deleteByPrimaryKeys", params);
-	}
+    /**
+     * @see com.easy.admin.dao.AllotRoleDao#deleteByPrimaryKeys(java.lang.Long,
+     *      java.lang.Long)
+     */
+    @Override
+    public int deleteByPrimaryKeys(Long userId, Long roleId) {
 
-	/**
-	 * @see com.easy.admin.dao.AllotRoleDao#selectByCriteria(com.easy.admin.entity.AllotRole)
-	 */
-	@Override
-	public List<AllotRole> selectByCriteria(AllotRole allotRole) {
-		return list("com.easy.dao.AllotRoleDao.selectByCriteria", allotRole);
-	}
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("userId", userId);
+        params.put("roleId", roleId);
+        return get("com.easy.dao.AllotRoleDao.deleteByPrimaryKeys", params);
+    }
+
+    /**
+     * @see com.easy.admin.dao.AllotRoleDao#selectByCriteria(com.easy.admin.entity.AllotRole)
+     */
+    @Override
+    public List<AllotRole> selectByCriteria(AllotRole allotRole) {
+        return list("com.easy.dao.AllotRoleDao.selectByCriteria", allotRole);
+    }
 
 }
